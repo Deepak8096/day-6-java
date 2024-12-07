1a3ed2ebc806407cfe19fe63a768dc7b9fdf7a15
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-07 15:37:10 +0530

    Add files via upload

---
+++ uniqarr.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+  public
+    int search(int n, int arr[]); {
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans^=arr[i];
+            return ans;
+        }
+    }
+}
